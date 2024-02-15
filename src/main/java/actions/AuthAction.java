a15a975ed767b00b53c8e7859d7738fcbef46197
maimi.ishizawa <demekinally@gmail.com>
2024-02-15 21:16:49 +0900

    ログインアクションの作成

--- src/main/java/actions/AuthAction.java
+++ src/main/java/actions/AuthAction.java
@@ -4,14 +4,16 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 
+import actions.views.EmployeeView;
 import constants.AttributeConst;
 import constants.ForwardConst;
+import constants.MessageConst;
+import constants.PropertyConst;
 import services.EmployeeService;
 
 public class AuthAction extends ActionBase {
 
     private EmployeeService service;
-
 
     public AuthAction() {
         // TODO 自動生成されたコンストラクター・スタブ
@@ -25,6 +27,7 @@ public class AuthAction extends ActionBase {
         service.close();
 
     }
+
     /**
      * ログイン画面を表示する
      * @throws ServletException
@@ -32,18 +35,58 @@ public class AuthAction extends ActionBase {
      */
     public void showLogin() throws ServletException, IOException {
         //CSRF対策用トークンを設定
-        putRequestScope(AttributeConst.TOKEN,getTokenId());
+        putRequestScope(AttributeConst.TOKEN, getTokenId());
 
         //セッションにフラッシュメッセージが登録されている場合はリクエストスコープに設定する
-        String flush =getSessionScope(AttributeConst.FLUSH);
+        String flush = getSessionScope(AttributeConst.FLUSH);
 
         if (flush != null) {
-//            putRequestScope(AttributeConst.FLUSH,flush);
+            //            putRequestScope(AttributeConst.FLUSH,flush);
             removeSessionScope(AttributeConst.FLUSH);
         }
 
         //ログイン画面を表示
         forward(ForwardConst.FW_LOGIN);
+    }
+
+    /**
+     * ログイン処理を行う
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void login() throws ServletException, IOException {
+        String code = getRequestParam(AttributeConst.EMP_CODE);
+        String plainPass = getRequestParam(AttributeConst.EMP_PASS);
+        String pepper = getContextScope(PropertyConst.PEPPER);
+
+        Boolean isValidEmployee = service.validateLogin(code, plainPass, pepper);
+
+        if (isValidEmployee) {
+            //認証成功の場合
+
+            if (checkToken()) {
+                //ログインした従業員のDBデータを取得
+                EmployeeView ev = service.findOne(code, plainPass, pepper);
+                //セッションにログインした従業員を設定
+                putSessionScope(AttributeConst.LOGIN_EMP, ev);
+                //セッションにログイン完了のフラッシュメッセージを設定
+                putSessionScope(AttributeConst.FLUSH, MessageConst.I_LOGINED.getMessage());
+                //トップページへリダイレクト
+                redirect(ForwardConst.ACT_TOP, ForwardConst.CMD_INDEX);
+            }
+        }else {
+            //認証失敗の場合
+            //CSRF対策用トークンを設定
+            putRequestScope(AttributeConst.TOKEN, getTokenId());
+            //認証失敗エラーメッセージ表示フラグをたてる
+            putRequestScope(AttributeConst.LOGIN_ERR, true);
+            //入力された従業員コードを設定
+            putRequestScope(AttributeConst.EMP_CODE, code);
+
+            //ログイン画面を表示
+            forward(ForwardConst.FW_LOGIN);
+
+        }
     }
 
 }
