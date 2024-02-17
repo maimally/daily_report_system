9f36ae14c3841ac384d9a64d8c473d480f1e21aa
maimi.ishizawa <demekinally@gmail.com>
2024-02-17 14:57:56 +0900

    Reportモデル、ReportViewモデル作成

---
+++ src/main/java/actions/views/ReportView.java
@@ -0,0 +1,27 @@
+package actions.views;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReportView{
+
+    private Integer id;
+
+    private EmployeeView employee;
+
+    private LocalDate reportDate;
+    private String title; // タイトル
+    private String content;//登録内容
+    private LocalDateTime createdAt;//登録日時
+    private LocalDateTime updatedAt;
+
+}
